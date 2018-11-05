56ba10ec75f78819ad1e5175dccb6cc1fbe0f2ad
AngelEllesse <angelellesse@googlemail.com>
2018-11-05 11:59:05 +0000

    14146615.v1
    CA to create animation with alternating images
    drag and move images

---
+++ Assignment_template/Assignment_template/ViewController.swift
@@ -0,0 +1,130 @@
+//
+//  ViewController.swift
+//  Assignment_template
+//
+//  Created by aw15abo on 05/11/2018.
+//  Copyright © 2018 CSStestuser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var road: UIImageView!
+    
+    @IBOutlet weak var tree: UIImageView!
+    
+    @IBOutlet weak var cloud: UIImageView!
+
+    @IBOutlet weak var cloud2: UIImageView!
+    
+    @IBOutlet weak var plane: dragged_image!
+    
+    let W = UIScreen.main.bounds.width
+    let H = UIScreen.main.bounds.height
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+        
+        var imageArrayRoad: [UIImage]!
+        
+        imageArrayRoad = [UIImage(named: "road1.png")!,
+                      UIImage(named: "road2.png")!,
+                      UIImage(named: "road3.png")!,
+                      UIImage(named: "road4.png")!,
+                      UIImage(named: "road5.png")!,
+                      UIImage(named: "road6.png")!,
+                      UIImage(named: "road7.png")!,
+                      UIImage(named: "road8.png")!,
+                      UIImage(named: "road9.png")!,
+                      UIImage(named: "road10.png")!,
+                      UIImage(named: "road11.png")!,
+                      UIImage(named: "road12.png")!,
+                      UIImage(named: "road13.png")!,
+                      UIImage(named: "road14.png")!,
+                      UIImage(named: "road15.png")!,
+                      UIImage(named: "road16.png")!,
+                      UIImage(named: "road17.png")!,
+                      UIImage(named: "road18.png")!,
+                      UIImage(named: "road19.png")!]
+        
+        road.image = UIImage.animatedImage(with: imageArrayRoad, duration: 1)
+        self.road.frame.size.width = W
+        self.road.frame.size.height = H
+        
+        
+        var imageArrayTree: [UIImage]!
+        
+        imageArrayTree = [UIImage(named: "tree1.png")!,
+                          UIImage(named: "tree2.png")!,
+                          UIImage(named: "tree3.png")!,
+                          UIImage(named: "tree4.png")!,
+                          UIImage(named: "tree5.png")!,
+                          UIImage(named: "tree6.png")!,
+                          UIImage(named: "tree7.png")!,
+                          UIImage(named: "tree8.png")!,
+                          UIImage(named: "tree9.png")!,
+                          UIImage(named: "tree10.png")!,
+                          UIImage(named: "tree11.png")!,
+                          UIImage(named: "tree12.png")!,
+                          UIImage(named: "tree13.png")!,
+                          UIImage(named: "tree14.png")!,
+                          UIImage(named: "tree15.png")!,
+                          UIImage(named: "tree16.png")!,
+                          UIImage(named: "tree17.png")!]
+        
+        tree.image = UIImage.animatedImage(with: imageArrayTree, duration: 3)
+        
+        self.tree.frame.size.width = W
+       // self.tree.frame.size.height = H * -1.1
+        
+        
+        var imageArrayPlane: [UIImage]!
+        
+        imageArrayPlane = [UIImage(named: "plane1.png")!,
+                          UIImage(named: "plane2.png")!,
+                          UIImage(named: "plane3.png")!,
+                          UIImage(named: "plane4.png")!,
+                          UIImage(named: "plane5.png")!,
+                          UIImage(named: "plane6.png")!,
+                          UIImage(named: "plane7.png")!,
+                          UIImage(named: "plane8.png")!,
+                          UIImage(named: "plane9.png")!,
+                          UIImage(named: "plane10.png")!,
+                          UIImage(named: "plane11.png")!,
+                          UIImage(named: "plane12.png")!,
+                          UIImage(named: "plane13.png")!,
+                          UIImage(named: "plane14.png")!,
+                          UIImage(named: "plane15.png")!]
+        
+        plane.image = UIImage.animatedImage(with: imageArrayPlane, duration: 1)
+
+        
+        
+        UIView.animate(withDuration: 40, delay: 0.0, options: [UIViewAnimationOptions.repeat, .curveLinear], animations:
+            {
+                self.cloud.center.x += self.view.bounds.width
+                self.cloud2.center.x += self.view.bounds.width
+                
+        }, completion: nil
+        )
+
+        
+
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Assignment_template/Assignment_template/dragged_plane.swift
@@ -0,0 +1,24 @@
+//
+//  dragged_plane.swift
+//  Cherry_Animation
+//
+//  Created by aw15abo on 05/11/2018.
+//  Copyright © 2018 Xianhui Che. All rights reserved.
+//
+
+import UIKit
+
+class dragged_plane: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    
+    
+
+}
